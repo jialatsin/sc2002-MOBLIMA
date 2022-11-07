c931c1d0476f543f5a7d15b03147f6a537c30ce7
29tushar <97389295+29tushar@users.noreply.github.com>
2022-11-08 07:45:31 +0800

    Update PriceType.java

--- moblima/src/entity/PriceType.java
+++ moblima/src/entity/PriceType.java
@@ -1,6 +1,11 @@
 package entity;
-
-/* Empty interface implemented by classes that will affect the price of a movie ticket */
+/**
+ * Empty interface implemented by classes that will affect the price of a movie ticket 
+ * @author OOP SSP1 Lab Group 4
+ * @version 7/11/2022
+ */
 public interface PriceType {
-    // Empty interface
+    /**
+     * Empty Interface
+     */
 }
